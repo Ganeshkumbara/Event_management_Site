b2bbe764a668ee5f3cd9bf21bee4dae70d08f4c7
Ganeshkumbara <ganesh700511@gmail.com>
2023-01-08 22:49:17 +0530

    Created Project and started with first app called Home

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from django.views.generic import RedirectView
+
+urlpatterns =[
+    path('', RedirectView.as_view(url='home/')),
+    path('home/', views.home, name='Home' )
+]
