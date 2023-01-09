ef5f3c7ac5e2db5aafa197f4ee9dce3b58681c7a
Ganeshkumbara <ganesh700511@gmail.com>
2023-01-09 23:08:49 +0530

    Added HtML and CSS  to djnaog project sucessfully, in setting.py defined static file path runned py manage.py collectstatic to let django create new file by adding all static files into newly created file

--- BEventSite/settings.py
+++ BEventSite/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [  
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assests') #django collect all the static files and save it in assests
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
