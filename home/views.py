1ace6a484862feae374e1ab8fabe89809d7f7ad0
Ganeshkumbara <ganesh700511@gmail.com>
2023-01-16 23:45:43 +0530

    automail respond to custmer query and whatsapp icon below products for order, by clicking on icon user will redirected to whatsapp with default text

--- home/views.py
+++ home/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.conf import settings
+from .models import Homepage
 
 def home(request):
-    return render(request, "index.html")
+
+    themes1 = Homepage()
+    themes1.name = "Blue_light princess "
+    themes1.price =  5500
+
+    themes2 = Homepage()
+    themes2.name = "Blue_light princess "
+    themes2.price =  5500
+    
+    lst = [themes1,themes2]
+    return render(request, "index.html",{"lst":lst})
+
+
+
+
+
 
 
 def email(request):
@@ -13,11 +29,12 @@ def email(request):
     username = request.POST['name']
 
     if request.method == "POST":
-        subject = 'welcome to GFG world'
-        message = f'Hi {username}, thanks showing interset will get back to u shortly.'
+        subject = 'Django test mail'
+        message = (f'Hi {username}, thanks showing interset will get back to u shortly..\n stayed tunned ')
         email_from = settings.EMAIL_HOST_USER
         recipient_list = [email]
         send_mail( subject, message, email_from, recipient_list )
+
         return redirect('/')
     return redirect('/')
      
